59b02eeab05cdbe1eead69299687bcc1f8845837
yhaaan <glesu@naver.com>
2023-01-16 23:18:31 +0900

    최대 최소 설정

--- Assets/4Scripts/MoveEffect.cs
+++ Assets/4Scripts/MoveEffect.cs
@@ -9,20 +9,30 @@ public class MoveEffect : MonoBehaviour
     [SerializeField]
     private float speed = 40;
     [SerializeField]
+    private float min = 0;
+    [SerializeField]
+    private float max = 100;
+    [SerializeField]
     private int effectCount = 5;
-    
+    private float Scale;
 
     private void Update()
     {
 
         while(effectCount > 0)
         {
+            Scale = SetScale(min, max, effectCount);
             GameObject clone = Instantiate(moveEffectPrefab, transform.position, Quaternion.identity);
-            clone.transform.localScale = new Vector3(100/effectCount *0.01f, 100 / effectCount * 0.01f, 100 / effectCount * 0.01f);
+            clone.transform.localScale = new Vector3(Scale, Scale,1);
             clone.GetComponent<FollowPlayer>().Setup(transform,speed/effectCount);
 
 
             effectCount--;
         }
+    }
+
+    private float SetScale(float min, float max, int effectCount)
+    {
+        return ((max-min)/effectCount+min)*0.01f;
     }
 }
