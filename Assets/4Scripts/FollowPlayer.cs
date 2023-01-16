f5d0ab19dc4dd87659fcf974fa1b6084c4d173fc
Yi Jiho <ljhmuse@gmail.com>
2023-01-16 20:28:35 +0900

    Update FollowPlayer.cs

--- Assets/4Scripts/FollowPlayer.cs
+++ Assets/4Scripts/FollowPlayer.cs
@@ -6,10 +6,10 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
-    public float speed;
+    public float lerpSpeed;
 
     private void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.position = Vector2.Lerp(transform.position, player.transform.position, lerpSpeed * Time.deltaTime);
     }
 }
