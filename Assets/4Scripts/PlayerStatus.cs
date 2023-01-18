b876eadb540960cc3d8bb8d55d201d9abec9496c
Yi Jiho <ljhmuse@gmail.com>
2023-01-19 03:58:30 +0900

    Hit

---
+++ Assets/4Scripts/PlayerStatus.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    /*[SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private GameObject pattern;*/
+    SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        //player = GetComponent<GameObject>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Pattern")
+        {
+            Debug.Log("Hit");
+            OnDamaged();
+        }
+    }
+
+    private void OnDamaged()
+    {
+        gameObject.layer = 11;
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        Invoke("OffDamage", 1);
+    }
+
+    private void OffDamaged()
+    {
+        gameObject.layer = 10;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+}
