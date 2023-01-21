91293974b8a6b686fc6af2408596b478dad40068
Yi Jiho <ljhmuse@gmail.com>
2023-01-21 20:26:07 +0900

    Pattern 03
    This reverts commit f679fc25d78699498ce0dd71a9462256749bd29c.

--- Assets/4Scripts/Pattern03.cs
+++ Assets/4Scripts/Pattern03.cs
@@ -9,11 +9,13 @@ public class Pattern03 : MonoBehaviour
     [SerializeField]
     private GameObject patternPrefab;
     [SerializeField]
+    private GameObject patternPrefabBomb;
+    [SerializeField]
     private GameObject gameManager;
     [SerializeField]
     private float spawnCycle;
 
-    private List<GameObject> clone = new List<GameObject>();
+    private List<GameObject> clone_patter03 = new List<GameObject>();
 
     private void OnEnable()
     {
@@ -31,20 +33,34 @@ public class Pattern03 : MonoBehaviour
 
         while (true)
         {
+            // ·£´ý ÁÂÇ¥ x, y°ª »ý¼º
             int x = Random.Range(-2, 3);
             int y = Random.Range(-2, 3);
 
+            Vector3 position_bomb = new Vector3(x, y, 0);
+
+            // ·£´ý ÁÂÇ¥ x, y¿¡ ÆøÅº ÀÌ¹ÌÁö »ý¼º
+            GameObject clone_bomb = Instantiate(patternPrefabBomb, position_bomb, Quaternion.identity);
+
+            // waittime ±â´Ù¸®°í
+            yield return new WaitForSeconds(waitTime);
+
+            // ÆøÅº ÀÌ¹ÌÁö Á¦°Å
+            Destroy(clone_bomb);
+
+            // ·£´ý ÁÂÇ¥ x, y±âÁØÀ¸·Î 3x3 Å©±âÀÇ ÆÐÅÏ »ý¼º
             for (int i = -1; i < 2; i++)
             {
                 for (int j = -1; j < 2; j++)
                 {
                     Vector3 position = new Vector3(x + i, y + j, 0);
-                    GameObject t = Instantiate(patternPrefab, position, Quaternion.identity);
-                    clone.Add(t);
+                    GameObject clone_pattern03 = Instantiate(patternPrefab, position, Quaternion.identity);
+                    clone_patter03.Add(clone_pattern03);
                 }
             }
             yield return new WaitForSeconds(spawnCycle);
-            foreach (GameObject go in clone)
+            // 3x3 ÆÐÅÏ Á¦°Å
+            foreach (GameObject go in clone_patter03)
             {
                 Destroy(go);
             }
